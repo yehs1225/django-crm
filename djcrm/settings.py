1a585814f37891862b6496f5b06465b027d0bed8
葉子瑄 <yeh.mentos@gmail.com>
2022-03-02 20:27:57 +0800

    Fix Static file (collectstatic)

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -125,9 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR/"static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT="static_root"
-STATIC_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT="static_root"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
